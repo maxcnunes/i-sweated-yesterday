25ee5ed790323081d59fe2e30839d21952f55a59
Max Claus Nunes <maxcnunes@gmail.com>
2012-12-20 00:11:26 -0200

    Added root route and new comments descriptions

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,9 +6,19 @@ app.config.from_object('config')
 
 db = SQLAlchemy(app)
 
+
+# Basic Routes #
+
+@app.route('/')
+def index():
+	return redirect(url_for('users.index'))
+
 @app.errorhandler(404)
 def not_found(error):
 	return render_template('404.html'), 404
+
+
+# BluePrints - Modules #
 
 # Users Module
 from app.users.views import mod as usersModule
@@ -17,9 +27,3 @@ app.register_blueprint(usersModule)
 # Exercises Module
 from app.exercises.views import mod as exercisesModule
 app.register_blueprint(exercisesModule)
-
-# Later on you'll import the other blueprints the same way:
-#from app.comments.views import mod as commentsModule
-#from app.posts.views import mod as postsModule
-#app.register_blueprint(commentsModule)
-#app.register_blueprint(postsModule)
