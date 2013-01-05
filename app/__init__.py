f172f1ebe7a2d9173ef37742b45fea95da8f72d4
Max Claus Nunes <maxcnunes@gmail.com>
2013-01-05 01:27:08 -0200

    Adding support to send emails

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, redirect, url_for
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.mail import Mail
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 db = SQLAlchemy(app)
 
+mail_sender = Mail(app)
 
 # Basic Routes #
 
