9775ab914ba2298c81dff38de91f62130fcc9d0a
Max Claus Nunes <maxcnunes@gmail.com>
2013-01-09 00:23:33 -0200

    Fixing length of password field, because was too small for a encripted value

--- app/users/models.py
+++ app/users/models.py
@@ -13,7 +13,7 @@ class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(50), unique=True)
 	email = db.Column(db.String(120), unique=True)
-	password = db.Column(db.String(20))
+	password = db.Column(db.String(100))
 	role = db.Column(db.SmallInteger, default=USER.USER)
 	status = db.Column(db.SmallInteger, default=USER.NEW)
 	email_exercise_token = db.Column(db.String(50))
